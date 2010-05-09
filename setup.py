635ae4a08028fe1e0d8d341d84d24d63c610f46a
Alex Lee <lee@ccs.neu.edu>
2010-05-09 13:30:20 -0700

    Update setup.py; Add classifiers

--- setup.py
+++ setup.py
@@ -4,16 +4,25 @@ from distutils.core import setup
 
 # FIXME version number, etc.
 setup(
-    name = 'dds-server',
-    description = 'Digital Display Jabber Server',
-    version = '0.0.1',
+    name = 'dds-harvest',
+    version = '0.1.0',
+    license = '',
+    description = 'Digital Display Harvest Server',
+    long_description = 'Digital Display Harvest Server',
     author = 'Alex Lee',
     author_email = 'lee@ccs.neu.edu',
     maintainer = 'Alex Lee',
     maintainer_email = 'lee@ccs.neu.edu',
-    #url = '',
+    url = 'http://crew-git.ccs.neu.edu/bugs/projects/harvest',
+    download_url = '',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: Education',
+    ],
+    platforms = ['linux'],
     scripts = ['scripts/dds-server'],
     packages = ['harvest'],
-    package_dir = {'' : ''},
     data_files = [('/etc', ['cfg/dds-server.conf']),],
 )
